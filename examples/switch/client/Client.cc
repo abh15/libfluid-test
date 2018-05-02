27b21c87a8c4ff24994323de4c597dc2248e19e3
abhishek <abhishek15@iitb.ac.in>
2018-05-02 17:13:43 +0530

    switch/cli fixed

--- examples/switch/client/Client.cc
+++ examples/switch/client/Client.cc
@@ -1,6 +1,15 @@
 #include "OFClient.hh"
+uint64_t generate_dp_id() {
+	srand (time(NULL));
+	return (((uint64_t) rand() <<  0) & 0x000000000000FFFFull) ^ 
+		  (((uint64_t) rand() << 16) & 0x00000000FFFF0000ull) ^ 
+		  (((uint64_t) rand() << 32) & 0x0000FFFF00000000ull) ^
+		  (((uint64_t) rand() << 48) & 0xFFFF000000000000ull);
+}
 
 int main() {
-    OFClient client(0, "127.0.0.1", 6653);
+    uint64_t dp_id = generate_dp_id();
+
+    OFClient client(0, "127.0.0.1", 6653,dp_id, OFServerSettings().supported_version(0x01));
     client.start(true);
 }
--- examples/switch/client/base/BaseOFClient.cc
+++ examples/switch/client/base/BaseOFClient.cc
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <signal.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <arpa/inet.h>
 
